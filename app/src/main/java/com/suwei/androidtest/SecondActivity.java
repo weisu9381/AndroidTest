022773cc0bc9eca021fe6b1a0c07f9f758eb3d32
weisu9381 <weisu9381@gmail.com>
2017-10-16 13:32:22 +0800

    初始提交

---
+++ app/src/main/java/com/suwei/androidtest/SecondActivity.java
@@ -0,0 +1,35 @@
+package com.suwei.androidtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("extra_return", "from SecondActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("extra_return", "From Second");
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
