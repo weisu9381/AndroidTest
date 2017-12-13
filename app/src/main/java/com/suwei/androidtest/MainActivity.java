2e13cffbd4bac299748535ad6e52960e9a7e85c1
suwei <suwei9381@gmail.com>
2017-12-13 08:52:46 +0800

    从今天开始看这篇文章: http://www.jianshu.com/p/5ad013eb5364

--- app/src/main/java/com/suwei/androidtest/MainActivity.java
+++ app/src/main/java/com/suwei/androidtest/MainActivity.java
@@ -1,15 +1,20 @@
 package com.suwei.androidtest;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 
 import com.suwei.androidtest.date_2017_10.Date201710Activity;
 import com.suwei.androidtest.date_2017_11.Date201711Activity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +33,63 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        new Test().test();
+        Log.e("MainActivity","onCreate");
+
+        TelephonyManager tm = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        Log.e(TAG,"DEVICE_ID="+tm.getDeviceId());
+
+        String str = Settings.System.getString(getContentResolver(), Settings.System.ANDROID_ID);
+        Log.e(TAG,"ANDROID_ID"+str);
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        Log.e("MainActivity","onConfigurationChanged");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.e("MainActivity","onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.e("MainActivity","onRestoreInstanceState");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("MainActivity","onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("MainActivity","onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("MainActivity","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("MainActivity","onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MainActivity","onDestory");
+
     }
 }
